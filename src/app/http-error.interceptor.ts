6393ed12ab0b1b0464a74cde601b3eeea3cfd357
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-07-24 20:27:12 +0300

    Create HTTP error handling

---
+++ src/app/http-error.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          console.error('404 Error: Resource not found');
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
